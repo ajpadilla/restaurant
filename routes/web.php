11e0b902b278612682d35323a820a383b04f9e92
Alvaro Padilla <ajpadilla88@gmail.com>
2023-10-12 09:34:25 -0500

    Agregando rutas del proyecto

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('orders', 'OrderController@create');
+Route::post('orders', 'OrderController@store')->name('orders.store');
+Route::get('orders/created', 'OrderController@index')->name('orders.index');
+Route::get('orders/process', 'OrderController@process')->name('orders.process');
+Route::get('plates', 'PlateController@index')->name('plates.index');
+Route::get('ingredients', 'IngredientController@index')->name('ingredients.index');
+Route::get('purchases', 'PurchaseController@index')->name('purchases.index');
+
