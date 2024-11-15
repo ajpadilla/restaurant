e1af3f2fd64cf9dbcf811034be43fb38b905d83f
Alvaro Padilla <ajpadilla88@gmail.com>
2024-11-15 12:22:04 -0500

    Instanciando respositoruios y servicios

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Repositories\Ingredient\EloquentIngredientRepository;
+use App\Repositories\Ingredient\IngredientRepository;
+use App\Repositories\Order\EloquentOrderRepository;
+use App\Repositories\Order\OrderRepository;
+use App\Repositories\Plate\EloquentPlateRepository;
+use App\Repositories\Plate\PlateRepository;
+use App\Services\Messaging\MessageProducerInterface;
+use App\Services\Messaging\RabbitMQ\OrderRabbitMQProducer;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(MessageProducerInterface::class, OrderRabbitMQProducer::class);
+        $this->app->bind(OrderRepository::class, EloquentOrderRepository::class);
+        $this->app->bind(PlateRepository::class, EloquentPlateRepository::class);
+        $this->app->bind(IngredientRepository::class, EloquentIngredientRepository::class);
     }
 
     /**
