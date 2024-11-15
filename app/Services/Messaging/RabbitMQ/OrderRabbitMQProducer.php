7a66a74f0eadd8eadee24b37c03a1cfa2dd40fe0
Alvaro Padilla <ajpadilla88@gmail.com>
2024-11-15 12:23:13 -0500

    Implementando servicio para envio de mensajes

--- app/Services/Messaging/RabbitMQ/OrderRabbitMQProducer.php
+++ app/Services/Messaging/RabbitMQ/OrderRabbitMQProducer.php
@@ -1,41 +1,38 @@
 <?php
 
-namespace App\Services\RabbitMQ;
+namespace App\Services\Messaging\RabbitMQ;
 
+use App\Services\Messaging\MessageProducerInterface;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-class OrderRabbitMQProducer
+class OrderRabbitMQProducer implements MessageProducerInterface
 {
     protected $connection;
     protected $channel;
 
     public function __construct()
     {
-        // Conecta con RabbitMQ (ajusta las credenciales según tu configuración)
-        $this->connection = new AMQPStreamConnection(env('RABBITMQ_HOST'), env('RABBITMQ_PORT'), env('RABBITMQ_USER'), env('RABBITMQ_PASSWORD'));
-        $this->channel = $this->connection->channel();
     }
 
-    public function sendMessage($orderData = [], $queue = 'order-created')
+    public function sendMessage(array $messageData, string $queue = 'order-created'): void
     {
-        // Reestablece la conexión y el canal antes de enviar el mensaje
+
         $this->connection = new AMQPStreamConnection(env('RABBITMQ_HOST'), env('RABBITMQ_PORT'), env('RABBITMQ_USER'), env('RABBITMQ_PASSWORD'));
         $this->channel = $this->connection->channel();
 
         // Convertimos los datos a formato JSON
-        $messageJson = json_encode($orderData);
+        $messageJson = json_encode($messageData);
 
         // Declara la cola y envía el mensaje
         $this->channel->queue_declare($queue, false, true, false, false);
         $msg = new AMQPMessage($messageJson, ['delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT]);
         $this->channel->basic_publish($msg, '', $queue);
 
-        \Log::info("Mensaje enviado a la cola {$queue}: " . $messageJson);
+        \Log::info("Mensaje enviado a la Cocina {$queue}: " . $messageJson);
 
         // Cierra el canal y la conexión después de enviar el mensaje
-        $this->channel->close();
-        $this->connection->close();
+       $this->channel->close();
+       $this->connection->close();
     }
-
 }
