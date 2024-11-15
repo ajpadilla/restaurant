c241cc4e4644367ff522ccf2bc408ab4eae22bd4
Alvaro Padilla <ajpadilla88@gmail.com>
2024-11-15 12:23:45 -0500

    Agregando servicio para estructura orden

--- app/Services/OrderIngredientService.php
+++ app/Services/OrderIngredientService.php
@@ -4,21 +4,19 @@ namespace App\Services;
 
 use App\Order;
 
-class OrderService
+class OrderIngredientService
 {
-
     public function __construct()
     {
     }
 
-
-    public function createIngredientListForKitchen(Order  $order): array
+    public function createIngredientListForKitchen(Order $order): array
     {
         $ingredientsListFromOrder = collect($order->plate->ingredients()->get()->toArray());
         $ingredients = $ingredientsListFromOrder->map(function ($item) {
-            return['name' => $item['name'], 'quantity' => $item['quantity']];
+            return ['name' => $item['name'], 'quantity' => $item['quantity']];
         })->toArray();
 
-        return['order_id' => $order->id, 'ingredients' => $ingredients];
+        return ['order_id' => $order->id, 'plate_id' => $order->plate->id, 'plate_name' => $order->plate->name , 'ingredients' => $ingredients];
     }
 }
