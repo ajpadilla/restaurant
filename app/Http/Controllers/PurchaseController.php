74f8831b1f8d9ba2edeccefdeb27927770eb329a
Alvaro Padilla <ajpadilla88@gmail.com>
2024-11-15 12:21:25 -0500

    Modificando respuesta del controlador

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -7,6 +7,8 @@ use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 
 class PurchaseController extends Controller
@@ -26,8 +28,18 @@ class PurchaseController extends Controller
      * @return Application|Factory|View
      * @throws GuzzleException
      */
-    public function index(Request $request) {
-        $purchases = $this->wareHouseClientService->getAllPurchases();
+    public function index(Request $request)
+    {
+        $page = $request->query('page', 1);
+
+        $pageSize = $request->query('pageSize', 10);
+
+        $allPurchases = $this->wareHouseClientService->getAllPurchases($page, $pageSize);
+
+        $totalResults = $allPurchases->total_results ?? count($allPurchases);
+
+        $purchases = new LengthAwarePaginator($allPurchases->purchases, $totalResults, $pageSize, $page);
+
         return view('layouts.pages.purchases', compact('purchases'));
     }
 }
